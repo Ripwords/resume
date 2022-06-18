9c5bc578bbba1fe58ea980d6654a2193e24b30e1
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2022-06-18 21:55:58 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="bg">
+  <div class="center">
+    <img src="/resume_Page_1.jpg" alt="resume_Page1">
+  </div>
+  <div class="center">
+    <img src="/resume_Page_2.jpg" alt="resume_Page2">
+  </div>
+</div>
+</template>
+
+<style>
+/* Matte gray background color with class named bg */
+.bg {
+  background-color: #727272;
+}
+
+.center {
+  display: flex;
+  justify-content: center;
+}
+
+img {
+  max-width: 900px;
+  width: 90%;
+  height: 100%;
+  object-fit: contain;
+}
+
+::-webkit-scrollbar {
+  width: 20px;
+}
+
+::-webkit-scrollbar-track {
+  background-color: transparent;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #d6dee1;
+  border-radius: 20px;
+  border: 6px solid transparent;
+  background-clip: content-box;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: #a8bbbf;
+}
+</style>
